2cec80b95c68976cc4456cc9b57a3a4c4972ff61
Mikel <mikel_cortina@euneiz.com>
2026-01-26 23:05:25 +0100

    Casi Arreglado

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -1,27 +1,45 @@
 using Photon.Pun;
-using System.Globalization;
 using UnityEngine;
 
 public class PlayerSetup : MonoBehaviourPunCallbacks
 {
     public Movement movement;
-    public GameObject playerCamera; // Cambié el nombre porque 'camera' es palabra reservada en versiones viejas
+    public GameObject playerCamera;
+
+    [Header("Objetos a ocultar para el dueño")]
+    public GameObject[] objectsToHide;
 
     void Start()
     {
-        // photonView.IsMine nos dice si este objeto es EL MÍO o el de otro jugador
         if (photonView.IsMine)
         {
-            // Soy yo: Activo mi movimiento y mi cámara
+            // Soy yo
             movement.enabled = true;
             playerCamera.SetActive(true);
+
+            // OCULTAR objetos que no quiero ver de mi propio cuerpo
+            foreach (GameObject obj in objectsToHide)
+            {
+                // Opción A: Desactivarlos por completo
+                // obj.SetActive(false); 
+
+                // Opción B: Solo ocultar el renderizado (mejor si tienen scripts o huesos)
+                if (obj.GetComponent<Renderer>() != null)
+                    obj.GetComponent<Renderer>().enabled = false;
+            }
         }
         else
         {
-            // Es otro jugador: Me aseguro de que su movimiento y cámara estén APAGADOS en mi pantalla
+            // Es otro jugador
             movement.enabled = false;
             playerCamera.SetActive(false);
-        }
 
+            // Asegurarse de que los otros jugadores SI vean mi cuerpo
+            foreach (GameObject obj in objectsToHide)
+            {
+                if (obj.GetComponent<Renderer>() != null)
+                    obj.GetComponent<Renderer>().enabled = true;
+            }
+        }
     }
 }
